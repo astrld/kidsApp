86a2dafb1c4012c2be5f9637612fd65c23fbe9ad
astrld <agrawal6595@students.d211.org>
2023-08-29 21:18:56 -0500

    ?

--- app/src/main/java/com/example/skeeballapp/MainActivity.java
+++ app/src/main/java/com/example/skeeballapp/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.skeeballapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout backgroundLayout;
+    private LinearLayout ballSpaceLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        backgroundLayout = findViewById(R.id.relativeLayout);
+        ballSpaceLayout = findViewById(R.id.layout2);
     }
 }
